8b9d2a5609c26d8a5f18ae029ee654999e258cd6
piromanser <piromanseix@gmail.com>
2019-03-25 16:26:59 +0300

    first commit

---
+++ slider.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow: '<button type="button" class="slick-nav">&larr;</button>',
+        nextArrow: '<button type="button" class="slick-nav">&rarr;</button>',
+    });
+  });
